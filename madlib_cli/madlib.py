f80586d33142a27d44f78671b80ed8221e00bd17
abdulelahxd <abdulelahkassara@gmail.com>
2020-08-18 19:16:31 +0300

    new commit

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,25 @@
+from textwrap import dedent
+
+
+def welcome():
+    message = """
+        **************************************
+        **       Welcome to Madlibs!        **
+        **    Please provide a response     **
+        **        to each question          **
+        **                                  **  
+        ** To quit at any time, type "quit" **
+        **************************************
+        """
+    print(dedent(message))
+
+
+def text_file():
+    with open("madlib.txt") as temp:
+        content = temp.read()
+
+    
+
+# Mad Libs are stories with words removed and replaced by blank spaces.  One player acts as the “reader” and asks the other players, who haven’t seen the story, to fill in the blanks with adjectives, nouns, exclamations, colors, adjectives, and more. These words are inserted into the blanks and then the story is read aloud to hilarious results.  There are no winners or losers, only laughter. Mad Libs Live! brings this simple game to the musical stage with a script and songs full of blanks that are filled in by audience members and an infectious pop score. Mad Libs Live! fully embraces the spirit of the “World’s Greatest Word Game” and is the perfect musical for audiences 7 and up who want to stop being serious and start getting goofy!
+# Today I went to the zoo. I saw a(n)
+ 
