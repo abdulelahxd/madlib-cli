68144dd00c2144af890e76958a91335873a6eef0
abdulelahxd <abdulelahkassara@gmail.com>
2020-08-19 05:43:29 +0300

    new update

--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,9 +1,17 @@
-def read_template():
-    
-    
-def parse():
-    
+from madlib_cli.madlib import madlib
 
-def merge():
-    
-    
+
+def test_go():
+    actual = madlib()
+    assert actual == """
+    Welcome to the Madlib cli app!
+    Prompts will ask you to input
+    certain types of words. For example,
+    if the prompt asks for a color you
+    might type "red". 
+    After you type your response press
+    enter to continue.
+    After all prompts have been entered, 
+    the finished madlib will be displayed 
+    and saved as a text file. Have fun!
+    """
